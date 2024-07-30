1b22571300925e08147b21e95dfff32ea663ac95
OfficialXquisite <abaraemmanuel57@gmail.com>
2024-07-30 13:42:52 +0100

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -6,15 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
-    $item = htmlspecialchars($_POST['item']);
-    $quantity = htmlspecialchars($_POST['quantity']);
-    $delivery = htmlspecialchars($_POST['delivery']);
+    $address = htmlspecialchars($_POST['address']);
+    $menuItem = htmlspecialchars($_POST['menu-item']);
+    $size = htmlspecialchars($_POST['size']);
+    $paymentMethod = htmlspecialchars($_POST['payment-method']);
     
-    // Save or process the order (e.g., store in database, send email)
     // For demonstration purposes, we're just echoing the values
     echo "<h1>Order Received</h1>";
-    echo "<p>Thank you, $name. Your order for $quantity of $item has been received.</p>";
-    echo "<p>Delivery method: $delivery</p>";
+    echo "<p>Thank you, $name. Your order for $size of $menuItem has been received.</p>";
+    echo "<p>Delivery Address: $address</p>";
+    echo "<p>Payment Method: $paymentMethod</p>";
     echo "<p>We will contact you at $phone or $email.</p>";
 } else {
     echo "<h1>Error</h1>";
